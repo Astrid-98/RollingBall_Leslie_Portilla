8288d153b6014a5e89878432c9575680f0f58eb0
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-11-02 22:31:46 +0100

    Correcciones

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody PlayerBola;
-    [SerializeField] Vector3 direccionF;
-    [SerializeField] int fuerza;
+    private Rigidbody PlayerBola;
+    [SerializeField] Vector3 direccionF; // Direccion del salto
+    [SerializeField] int fuerzaSalto;
     float h;
     float v;
 
     [SerializeField] private float fuerzaMovimiento;
     [SerializeField] private float distanciaRaycast;
 
+    int vidas = 10;
+    Vector3 posicionInicial;
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerBola = GetComponent<Rigidbody>();
+
+        posicionInicial = transform.position;
     }
 
     // Update is called once per frame
@@ -27,13 +33,13 @@ public class Player : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical"); // W y S para vertical
         Vector3 movimiento = new Vector3(h, 0, v).normalized;
 
-        PlayerBola.AddForce(movimiento * fuerza, ForceMode.Force);
+        PlayerBola.AddForce(movimiento * fuerzaMovimiento, ForceMode.Force);
 
         if (Input.GetKeyDown(KeyCode.Space)) // al precionar Space aplica salto
         {
             if (detectaSuelo() == true)// mira a ver si detecta el suelo
             {
-                PlayerBola.AddForce(direccionF * fuerza, ForceMode.Impulse);
+                PlayerBola.AddForce(direccionF * fuerzaSalto, ForceMode.Impulse);
             }
             
         }
@@ -49,6 +55,18 @@ public class Player : MonoBehaviour
         if (other.gameObject.CompareTag("Coleccionable"))
         {
             Destroy(other.gameObject);
+        }
+        else if(other.gameObject.CompareTag("Trampa"))
+        {
+            Destroy(other.gameObject);
+            if (vidas > 0)
+            {
+                vidas--;
+            } 
+        }
+        else if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            transform.position = posicionInicial;
         }
     }
     private bool detectaSuelo()
